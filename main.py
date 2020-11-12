0dd641425a9fb76f338f2aa9b83b3e7fe8931f31
Kender Mendoza <kendermendoza591@outlook.com>
2020-11-11 22:09:47 -0600

    Feature btn order

--- main.py
+++ main.py
@@ -10,12 +10,45 @@ class Window(QMainWindow):
         uic.loadUi("./views/mainWindow.ui", self)
         self.timer = Chronometer(5, self.min, self.sec)
 
-        # events of the btn
-        self.iniciar.clicked.connect(self.timer.init)
-        self.pausar.clicked.connect(self.timer.pause)
-        self.reiniciar.clicked.connect(self.timer.restart)
+        self.reiniciar.setEnabled(False)
+        self.pausar.setVisible(False)
+        self.saltar.setVisible(False)
 
-            
+        # events of the btn
+        self.iniciar.clicked.connect(self._pushInit)
+        self.pausar.clicked.connect(self._pushPause)
+        self.reiniciar.clicked.connect(self._pushRestart)
+        self.saltar.clicked.connect(self._pushSkip)
+
+    def _pushInit(self):
+        self.iniciar.setVisible(False)
+        self.pausar.setVisible(True)
+        self.reiniciar.setEnabled(True)
+        self.reiniciar.setVisible(True)
+        self.saltar.setVisible(False)
+        self.timer.init()
+    
+    def _pushPause(self):
+        self.iniciar.setText("Continuar")
+        self.iniciar.setVisible(True)
+        self.pausar.setVisible(False)
+        self.saltar.setVisible(True)
+        self.reiniciar.setVisible(False)
+        self.timer.pause()
+
+    def _pushRestart(self):
+        self.iniciar.setText("Iniciar")
+        self.iniciar.setVisible(True)
+        self.reiniciar.setEnabled(False)
+        self.pausar.setVisible(False)
+        self.saltar.setVisible(False)
+        self.timer.restart()
+
+    def _pushSkip(self):
+        self._pushRestart()
+        self.reiniciar.setVisible(True)
+        # se debe de aumentar el pomodoro        
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Window()
