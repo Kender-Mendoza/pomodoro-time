873ff4be035b2f2da485da83c42aa320a1951775
Kender <kendermendoza591@gmail.com>
2020-11-27 19:38:03 -0600

    View Counter

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("./views/mainWindow.ui", self)
-        self.timer = Chronometer(5, self.min, self.sec)
+
+        self.POMODORO = 60
+        self.SHORT_BREAK = 5
+        self.LONG_BREAK = 15
+        
+        self.timer = Chronometer(3, self.min, self.sec)
 
         self.reiniciar.setEnabled(False)
         self.pausar.setVisible(False)
@@ -26,7 +31,7 @@ class Window(QMainWindow):
         self.reiniciar.setEnabled(True)
         self.reiniciar.setVisible(True)
         self.saltar.setVisible(False)
-        self.timer.init()
+        self.timer.init(self._nextTimer)
     
     def _pushPause(self):
         self.iniciar.setText("Continuar")
@@ -48,7 +53,22 @@ class Window(QMainWindow):
         self._pushRestart()
         self.reiniciar.setVisible(True)
         # se debe de aumentar el pomodoro        
-        
+    
+    def _nextTimer(self, number):
+        # se reordenan los botonerç
+        self.iniciar.setText("Iniciar")
+        self.iniciar.setVisible(True)
+        self.reiniciar.setEnabled(False)
+        self.pausar.setVisible(False)
+        self.saltar.setVisible(False)
+
+        # se verifica que el numero para mostrarlo
+        if((number%2) != 0): # Numeros pares
+            self.counter.setText(str(number))
+
+        # se verifica el numero para ver el proximo timer
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Window()
--- modules/Chronometer.py
+++ modules/Chronometer.py
@@ -4,47 +4,54 @@ class Chronometer():
     def __init__(self, minute, labelMin, labelSec):
         self.minute = minute
         self.second = 0
+        self.timer = 0
+        self.count = 0
+
         self.labelMin = labelMin
         self.labelSec = labelSec
-        self.timer = 0
-
+        
         # Initializing the chronometer of interface
         self._initView()
 
         # Number of seconds
-        self.SECONDS = 59
+        self.SECONDS = 2
         self.MINUTES = minute
             
-    def _start(self, minute, second):
+    def _start(self, minute, second, callback):
         if minute >= 0: 
             def updateView():
                 if second > 0:
-                    self._start(minute, second - 1)
+                    self._start(minute, second - 1, callback)
                     self.minute = minute
-                    self.second = second             
+                    self.second = second  
+
                     self.labelSec.setText(("0"+ str(second)) if second < 10 else str(second))
                     self.labelMin.setText(("0"+ str(minute)) if minute < 10 else str(minute))
-                    #print(("0"+ str(minute)) if minute < 10 else str(minute), ("0"+ str(second)) if second < 10 else str(second))
+                    print(("0"+ str(minute)) if minute < 10 else str(minute), ("0"+ str(second)) if second < 10 else str(second))
 
                 else:
-                    self._start(minute - 1, self.SECONDS)
+                    self._start(minute - 1, self.SECONDS, callback)
                     self.minute = minute
                     self.second = second         
+                    
                     self.labelMin.setText(("0"+ str(minute)) if minute < 10 else str(minute))
                     self.labelSec.setText(("0"+ str(second)) if second < 10 else str(second))
-                    #print(("0"+ str(minute)) if minute < 10 else str(minute), ("0"+ str(second)) if second < 10 else str(second))
+                    print(("0"+ str(minute)) if minute < 10 else str(minute), ("0"+ str(second)) if second < 10 else str(second))
 
             self.timer = Timer(1, updateView)
             self.timer.start()
         else:
             self.timer.cancel()
-            return 1 # this is for indicate that one pomodoro is finishing
+            self.count += 1
+            print(self.count) 
+            callback(self.count)
+            return
 
     def _initView(self):
         self.labelMin.setText(("0"+ str(self.minute)) if self.minute < 10 else str(self.minute))
         self.labelSec.setText(("0"+ str(self.second)) if self.second < 10 else str(self.second))
 
-    def init(self):
+    def init(self, callback):
         if self.second == -1 : 
             self.second = 0
         elif self.second == 0 : 
@@ -53,7 +60,7 @@ class Chronometer():
         else : 
             self.second = self.second - 1
 
-        self._start(self.minute, self.second)
+        self._start(self.minute, self.second, callback)
 
     def pause(self):
         self.timer.cancel()
@@ -64,4 +71,4 @@ class Chronometer():
         self.second = 0 
         self.minute = self.MINUTES 
         # Reinitialize the chronometer's view
-        self._initView()        
+        self._initView()   
