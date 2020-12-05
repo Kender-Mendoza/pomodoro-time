8f64b4735dc1fb0bca3428fa3b40e4868da0f13e
Kender <kendermendoza591@gmail.com>
2020-12-05 17:38:10 -0600

    Feature-chronometer

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
 import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
+
 from modules.Chronometer import Chronometer
+
+import threading
+from concurrent.futures import Future
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
 
 # this class will create the main window
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("./views/mainWindow.ui", self)
-        self.timer = Chronometer(2, self.min, self.sec)
-        self.pausar.clicked.connect(self.timer.pause)
+        
+        self.timer = Chronometer(2)
+        self.pause.clicked.connect(self._test)
+
+        # Async
+        self.thread = threading.Thread(
+            target= self.timer.start,
+            args=(self._updateChronometerView, ),
+            daemon=True
+        )
+        self.thread.start()
+
+    def _updateChronometerView(self,minute,second):
+        self.min.setText(str(minute))
+        self.sec.setText(str(second))
+        logging.info(f'{minute}:{second}')
+
+    def _test(self):
+        logging.info("Estoy saludando de forma asyncrona")
+        
+
+    # esta funcion esperara el resultado del cronometro
+    def _chronometerFinish(self,p):
+        result = p.result()
+        if result%2 != 0:
+            self.counter.setText(str(p.result()))
+            print(format(p.result()))
             
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Window()
--- modules/Chronometer.py
+++ modules/Chronometer.py
@@ -1,34 +1,28 @@
 from threading import Timer
+from time import sleep
 
 class Chronometer():
-    def __init__(self, minute, labelMin, labelSec):
-        self.minute = minute
-        self.second = 0
-        self.labelMin = labelMin
-        self.labelSec = labelSec
-        self.timer = 0
-        self.labelMin.setText(("0"+ str(minute)) if minute < 10 else str(minute))
-        self.labelSec.setText("00")
+    def __init__(self, minutes):
+        self.MINUTES = minutes
+        self.SECOND = 59
 
-        self.start(self.minute, self.second)
-            
-    def start(self, minute, second):
-        if second >= 0:
-            def updateView():
-                self.second = second             
-                self.labelSec.setText(("0"+ str(second)) if second < 10 else str(second))
-                self.start(minute, second - 1)
+        self.min = 0
+        self.sec = 0
 
-            self.timer = Timer(1, updateView)
-            self.timer.start()
-        elif minute >= 0:
-            self.minute = minute
-            self.labelMin.setText(("0"+ str(minute)) if minute < 10 else str(minute))
-            self.start(minute - 1, 59)
-        else:
-            return 1 # this is for indicate that one pomodoro is finishing
 
-    def pause(self):
-        self.timer.cancel()
-        print("info", self.minute, self.second)
-            
+    def start(self, callback):
+        minutes = self.MINUTES
+        seconds = 0
+        
+        print(minutes,seconds)
+        while minutes >= 0:
+            while seconds >= 0:
+                callback(minutes, seconds)
+                sleep(1)
+
+                seconds -= 1
+
+            minutes -= 1
+            seconds = self.SECOND
+
+        return 1
